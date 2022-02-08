616b078c8ecaaa820738ad3beb502c0cc38b0663
BAUDINO PIERRE <PIE.BAUDINO@pedago.mermoz.edu>
2022-02-08 16:01:40 +0100

    initialisation travail à rendre

---
+++ accueil.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <ul>
+    <li><a href="souris.php">Souris</a></li>
+    <li><a href="robot.php">Robot</a></li>
+    <li><a href="alteres.php">Altères</a></li>
+    <li><a href="casque.php">Casque</a></li>
+    <li><a href="chaqussures.php">Chaussures</a></li>
+    <li><a href="bracelet.php">Bracelet</a></li>
+    <li><a href="montre.php">Montre</a></li>
+    </ul>
+
+</body>
+</html>
