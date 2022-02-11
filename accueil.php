761344603b3d6c985c8f471cd3854301d96e0a84
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-02-11 10:49:45 +0100

    fin commentaires, fin projet

--- accueil.php
+++ accueil.php
@@ -1,33 +1,49 @@
 <?php
 include('session.php');
 
+//instanciation d'un tableau
 $numArticle = array();
+//instanciation d'une constante
+const NMB_SEES=3;
 
+//si il y a eu une visite
 if (isset($_GET['retour'])) {
+    //j'attrappe la visite
     $retour = $_GET['retour'];
 
+    //j'insère mon timestamp actuel dans le tableau instancié au début
     $numArticle[] = $actuel;
 
+    //si ma session n'est pas instancier,
+    //alors ma session, ici que j'appelle test, est un tableau
     if (!isset($_SESSION['test'])) {
         $_SESSION['test'] = array();
     }
+    //j'attrappe mon tableau quad je lie à une variable
     $message = $_SESSION['test'];
 
+    //si ma session est instancié
     if (isset($_SESSION['test'])) {
+        //je regarde dans mon tableau si le nom de la page retourné est présente
+        //si c'est le cas, on ne modifie que le timestamp 
         if ($lookfor = array_search($retour, $message)) {
             $_SESSION['test'][$retour] = $actuel;
         }
         $_SESSION['test'][$retour] = $actuel;
     }
+    //si ma session est instancié mais n'a pas de valeur insérée.
+    //condition de ma première visite
     if (!isset($_SESSION['test']) && is_array($_SESSION['test'])) {
         $_SESSION['test'] = $retour;
     }
 
-
+    //j'attrappe mon tableau de visites, dont j'inverse le sens
     $message = array_reverse($_SESSION['test']);
+    //je range mon tableau selon le timestamp
     arsort($message);
-    while (count($message) > 3) {
-
+    //je compte le nombre de valeurs définie par ma costante dans le tableau
+    while (count($message) > NMB_SEES) {
+        //j'enlève le reste tant que je suis audessus de ma constante
         $longueur = array_pop($message);
     }
 }
@@ -60,10 +76,14 @@ if (isset($_GET['retour'])) {
 
     
     <?php
+    //si il a eu une visite
     if (isset($_GET['retour'])) {
+        //je compte le nombre de messages affichés
         $numSees=count($message);
+        //j'instancie 2 variables correspondantes
         $dernier = 'dernier article visité:';
         $derniers= 'derniers articles visités:';
+        //je concatène la varibla à mon compteur correspondant
         if($numSees==1)
         {
             $numSees.=" ".$dernier;
@@ -72,7 +92,9 @@ if (isset($_GET['retour'])) {
         {
             $numSees.=" ".$derniers;
         }
+        //j'affiche dans mon titre s'il y a eu une visite
         echo "<h2 />$numSees</h2>";
+        //j'affiche mon historique de visites de pages
         if (isset($_SESSION['test'])) {
             foreach ($message as $key => $value) {
                 echo "<br />" . $key . " : " . $value . "<br />";
