3eef514d7943e15bcfdf0c9ce0fda50dae1927f3
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-02-11 10:31:41 +0100

    fin code, reste : ajout commentaires

--- accueil.php
+++ accueil.php
@@ -1,72 +1,50 @@
 <?php
 include('session.php');
 
-$numArticle=array();
+$numArticle = array();
 
-if(isset($_GET['retour']))
-    {
-        $retour=$_GET['retour'];
+if (isset($_GET['retour'])) {
+    $retour = $_GET['retour'];
 
+    $numArticle[] = $actuel;
 
-        $numArticle[]=$actuel;
-
-        var_dump($numArticle);
-
-        
-
-        if(!isset($_SESSION['test']))
-        {
-            $_SESSION['test']=array();
-           
-        }
-        $message=$_SESSION['test'];
-  
-        $lookfor = array_search($retour,$message);
-        echo "<br />".$lookfor."<br />";
-
-        if(isset($_SESSION['test']))
-        {
-            if ($lookfor = array_search($retour,$message)) {
-                echo $lookfor;
-            }
-
-            $_SESSION['test'][]=$retour;
-
-        }
-        if(!isset($_SESSION['test']) && is_array($_SESSION['test']))
-        {
-            $_SESSION['test']=$retour;
-
-        }
-        
-        
-        $message=array_reverse($_SESSION['test']);
-        while(count($message)>3)
-        {
-
-            $longueur=array_pop($message);
-        }
-       var_dump($message);
-
-        foreach ($message as $key=> $value)
-        {
-            echo "<br />".$value."<br />";
-
-        }
-
+    if (!isset($_SESSION['test'])) {
+        $_SESSION['test'] = array();
     }
+    $message = $_SESSION['test'];
+
+    if (isset($_SESSION['test'])) {
+        if ($lookfor = array_search($retour, $message)) {
+            $_SESSION['test'][$retour] = $actuel;
+        }
+        $_SESSION['test'][$retour] = $actuel;
+    }
+    if (!isset($_SESSION['test']) && is_array($_SESSION['test'])) {
+        $_SESSION['test'] = $retour;
+    }
+
+
+    $message = array_reverse($_SESSION['test']);
+    arsort($message);
+    while (count($message) > 3) {
+
+        $longueur = array_pop($message);
+    }
+}
 
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
     <ul>
@@ -81,9 +59,28 @@ if(isset($_GET['retour']))
 
 
     
-    <?php 
-
+    <?php
+    if (isset($_GET['retour'])) {
+        $numSees=count($message);
+        $dernier = 'dernier article visité:';
+        $derniers= 'derniers articles visités:';
+        if($numSees==1)
+        {
+            $numSees.=" ".$dernier;
+        }
+        else
+        {
+            $numSees.=" ".$derniers;
+        }
+        echo "<h2 />$numSees</h2>";
+        if (isset($_SESSION['test'])) {
+            foreach ($message as $key => $value) {
+                echo "<br />" . $key . " : " . $value . "<br />";
+            }
+        }
+    }
     ?>
 
 </body>
+
 </html>
